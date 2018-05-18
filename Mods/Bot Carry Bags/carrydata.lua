d00a7589c13515c76aa23e4e722895a130cc0bfe
Dr_Newbie <tf2baidonation@gmail.com>
2018-05-18 20:30:48 +0800

    Bot Carry Bags[15]

--- Mods/Bot Carry Bags/carrydata.lua
+++ Mods/Bot Carry Bags/carrydata.lua
@@ -20,7 +20,7 @@ Hooks:PostHook(CarryData, "unlink", "BotCarry_CarryData_unlink", function(self)
 end)
 
 Hooks:PostHook(CarryData, "update", "BotCarry_CarryData_update", function(self)
-	if not self._bot_cannot_carry and not self._linked_to and self._unit:interaction() and self._unit:interaction():active() then
+	if not self._bot_cannot_carry and self._unit:interaction() and self._unit:interaction():active() then
 		local _all_AI_criminals = managers.groupai:state():all_AI_criminals() or {}	
 		for _, data in pairs(_all_AI_criminals) do
 			if data.unit and alive(data.unit) then
@@ -29,7 +29,6 @@ Hooks:PostHook(CarryData, "update", "BotCarry_CarryData_update", function(self)
 				else
 					if not data.unit:movement():carrying_bag() then
 						self:link_to(data.unit)
-						self._linked_to = data.unit
 						data.unit:movement():set_carrying_bag(self._unit)
 						break
 					end
