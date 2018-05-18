50463d904eecb71f0bf48ca4106e881f349ce3db
Dr_Newbie <tf2baidonation@gmail.com>
2018-05-19 00:03:17 +0800

    Bot Carry Bags[20]

--- Mods/Bot Carry Bags/playermanager.lua
+++ Mods/Bot Carry Bags/playermanager.lua
@@ -3,7 +3,11 @@ local BotCarryBags_PlayerManager_server_drop_carry = PlayerManager.server_drop_c
 function PlayerManager:server_drop_carry(...)
 	local carry_unit = BotCarryBags_PlayerManager_server_drop_carry(self, ...)
 	if carry_unit and alive(carry_unit) and carry_unit:carry_data() then
-		carry_unit:carry_data()._bot_can_carry = true	
+		if tweak_data.carry and tweak_data.carry[carry_unit:carry_data()._carry_id].is_corpse then
+			
+		else
+			carry_unit:carry_data()._bot_can_carry = true
+		end
 	end
 	return carry_unit
 end
