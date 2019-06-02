b5dec4cd408144cb4c6dde810a8faa400d7abfaf
Dr_Newbie <tf2baidonation@gmail.com>
2019-06-03 02:12:48 +0800

    WAC_RAIDWW2[5]

--- Mods/WAC_RAIDWW2/Menu/MyMenu.lua
+++ Mods/WAC_RAIDWW2/Menu/MyMenu.lua
@@ -8,7 +8,8 @@ if not WACApplyMyMenu and RequiredScript == "lib/managers/menu/menucomponentmana
 	function WACApplyMyMenu:reset()
 		self._settings = {
 			_enable = true,
-			_apply = {}
+			_apply = {},
+			_applyWpnHold = {}
 		}
 	end
 	
@@ -35,9 +36,7 @@ if not WACApplyMyMenu and RequiredScript == "lib/managers/menu/menucomponentmana
 				break
 			end
 		end
-		if not is_new then
-			
-		else
+		if is_new then
 			local _file = io.open(self.ModPath.."Menu/ApplySet.lua", "w+")
 			if _file and tweak_data and tweak_data.weapon and tweak_data.weapon.factory and tweak_data.weapon.factory.parts then
 				_file:write('Hooks:PostHook(WeaponFactoryTweakData, "init", "F_"..Idstring("Post.WeaponFactoryTweakData.init.WAC"):key(), function(self) \n')
@@ -63,6 +62,32 @@ if not WACApplyMyMenu and RequiredScript == "lib/managers/menu/menucomponentmana
 					end
 				end
 				_file:write('end) \n')
+				_file:close()
+			end
+		end
+		is_new = false
+		for ii_fid, dd_fdd in pairs(WACApplyMyMenu._settings._applyWpnHold) do
+			for iii_fid, ddd_fdd in pairs(dd_fdd) do
+				if ddd_fdd == true then
+					is_new = true
+					break
+				end
+			end
+			if is_new then
+				break
+			end
+		end
+		if is_new then
+			local _file = io.open(self.ModPath.."Menu/ApplyWpnHoldSet.lua", "w+")
+			if _file and tweak_data and tweak_data.weapon then
+				for ii_fid, dd_fdd in pairs(WACApplyMyMenu._settings._applyWpnHold) do
+					for iii_fid, ddd_fdd in pairs(dd_fdd) do
+						if ddd_fdd == true then
+							_file:write('tweak_data.weapon["'..ii_fid..'"].weapon_hold = "'..iii_fid..'" \n')
+							break
+						end
+					end
+				end
 				_file:close()
 			end
 		end
@@ -108,6 +133,13 @@ if not WACApplyMyMenu and RequiredScript == "lib/managers/menu/menucomponentmana
 						table.insert(_udd_apply[_a_obj], www)
 						_udd_uapply[www] = _a_obj
 					end
+					local _weapon_hold = {}
+					for weapon_id, weapon_data in pairs(tweak_data.weapon) do
+						if type(weapon_data) == "table" and weapon_data.weapon_hold and not _weapon_hold[weapon_data.weapon_hold] then
+							_weapon_hold[weapon_data.weapon_hold] = weapon_data.weapon_hold
+							new_loc["Loc_"..Idstring('WACApplyMyMenuFN__WpnHold__'..weapon_data.weapon_hold):key()] = weapon_data.weapon_hold
+						end
+					end
 					for weapon_id, weapon_data in pairs(tweak_data.weapon) do
 						if weapon_data.name_id and weapon_data.AMMO_PICKUP then
 							local _fid = managers.weapon_factory:get_factory_id_by_weapon_id(weapon_id)
@@ -123,6 +155,24 @@ if not WACApplyMyMenu and RequiredScript == "lib/managers/menu/menucomponentmana
 											_file:write('if not WACApplyMyMenu then return end \n')
 											_file:write(_w_class..' = '.._w_class..' or class(BLTMenu) \n')
 											_file:write('RaidMenuHelper:CreateMenu({name = "'.._w_class..'__Menu", name_id = "'..weapon_data.name_id..'", inject_menu = "WACApplyMyMenuMenu", class = '.._w_class..'}) \n')
+											--
+											--Weapon Hold
+											local _r_class = "T_"..Idstring('WACApplyMyMenuR__'..weapon_id):key()
+											_file:write(_r_class..' = '.._r_class..' or class(BLTMenu) \n')
+											_file:write('RaidMenuHelper:CreateMenu({name = "'.._r_class..'__FMenu", name_id = "'.."Loc_"..Idstring('WACApplyMyMenuFN__WpnHold'):key()..'", inject_menu = "'.._w_class..'__Menu", class = '.._r_class..'}) \n')
+											_file:write('function '.._r_class..':Init() \n')
+											_file:write('	WACApplyMyMenu = WACApplyMyMenu or {} \n')
+											_file:write('	WACApplyMyMenu._settings = WACApplyMyMenu._settings or {} \n')
+											_file:write('	WACApplyMyMenu._settings._applyWpnHold = WACApplyMyMenu._settings._applyWpnHold or {} \n')
+											_file:write('	WACApplyMyMenu._settings._applyWpnHold["'..weapon_id..'"] = WACApplyMyMenu._settings._applyWpnHold["'..weapon_id..'"] or {} \n')
+											for _, _hold in pairs(_weapon_hold) do
+												local _wh_name = "T_"..Idstring('WACApplyMyMenuWpnHold__'..weapon_id..'-->'.._hold):key()
+												_file:write('	WACApplyMyMenu._settings._applyWpnHold["'..weapon_id..'"]["'.._hold..'"] = WACApplyMyMenu._settings._applyWpnHold["'..weapon_id..'"]["'.._hold..'"] or false \n')
+												_file:write('	self:Toggle({ name = "'.._wh_name..'", text = "'.."Loc_"..Idstring('WACApplyMyMenuFN__WpnHold__'.._hold):key()..'", value = WACApplyMyMenu._settings._applyWpnHold["'..weapon_id..'"]["'.._hold..'"] and true or false, callback = function() if WACApplyMyMenu._settings._applyWpnHold["'..weapon_id..'"]["'.._hold..'"] then WACApplyMyMenu._settings._applyWpnHold["'..weapon_id..'"]["'.._hold..'"] = false else WACApplyMyMenu._settings._applyWpnHold["'..weapon_id..'"]["'.._hold..'"] = true end for i, d in pairs(WACApplyMyMenu._settings._applyWpnHold["'..weapon_id..'"]) do if i ~= "'.._hold..'" then WACApplyMyMenu._settings._applyWpnHold["'..weapon_id..'"][i] = false end end end }) \n')
+											end
+											_file:write('end \n')
+											--
+											--
 											for _, _u_fid in pairs(_udd) do
 												local _new_loc_add = "Loc_"..Idstring('WACApplyMyMenuFN__'.._u_fid):key()
 												local _f_class = "T_"..Idstring('WACApplyMyMenuF__'.._u_fid):key()
@@ -138,7 +188,7 @@ if not WACApplyMyMenu and RequiredScript == "lib/managers/menu/menucomponentmana
 														local _new_loc_xadd = "Loc_"..Idstring('WACApplyMyMenuFN__'.._xu_fid):key()
 														self._settings._apply[_u_fid] = self._settings._apply[_u_fid] or {}
 														self._settings._apply[_u_fid][_xu_fid] = self._settings._apply[_u_fid][_xu_fid] or false
-														_r_name = "T_"..Idstring('WACApplyMyMenuApply__'.._u_fid..'-->'.._xu_fid):key()
+														local _r_name = "T_"..Idstring('WACApplyMyMenuApply__'.._u_fid..'-->'.._xu_fid):key()
 														_file:write('	WACApplyMyMenu._settings._apply["'.._u_fid..'"]["'.._xu_fid..'"] = WACApplyMyMenu._settings._apply["'.._u_fid..'"]["'.._xu_fid..'"] or false \n')
 														_file:write('	self:Toggle({ name = "'.._r_name..'", text = "'.._new_loc_xadd..'", value = WACApplyMyMenu._settings._apply["'.._u_fid..'"]["'.._xu_fid..'"] and true or false, callback = function() if WACApplyMyMenu._settings._apply["'.._u_fid..'"]["'.._xu_fid..'"] then WACApplyMyMenu._settings._apply["'.._u_fid..'"]["'.._xu_fid..'"] = false else WACApplyMyMenu._settings._apply["'.._u_fid..'"]["'.._xu_fid..'"] = true end for i, d in pairs(WACApplyMyMenu._settings._apply["'.._u_fid..'"]) do if i ~= "'.._xu_fid..'" then WACApplyMyMenu._settings._apply["'.._u_fid..'"][i] = false end end end }) \n')
 													end
@@ -157,6 +207,7 @@ if not WACApplyMyMenu and RequiredScript == "lib/managers/menu/menucomponentmana
 					if _loc_file then
 						new_loc["Loc_"..Idstring('WACApplyMyMenuFN__Invisible'):key()] = 'Invisible'
 						new_loc["Loc_"..Idstring('WACApplyMyMenuFN__Forced'):key()] = 'Forced'
+						new_loc["Loc_"..Idstring('WACApplyMyMenuFN__WpnHold'):key()] = 'Weapon Hold'
 						_loc_file:write(json.encode(new_loc))
 						_loc_file:close()
 					end
