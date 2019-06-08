079df8feb4fe38deca6597b61a17536000370f86
Dr_Newbie <tf2baidonation@gmail.com>
2019-06-09 00:01:06 +0800

    AutoPickupAmmo[4]

--- Mods/AutoPickupAmmo/playerstandard.lua
+++ Mods/AutoPickupAmmo/playerstandard.lua
@@ -1,24 +1,23 @@
-function PlayerStandard:_find_pickups(t, list)
-	local pickups = World:find_units_quick("sphere", self._unit:movement():m_pos(), 200)
-	for _, pickup in ipairs(pickups) do
-		if pickup:interaction() and pickup:interaction().tweak_data and list[pickup:interaction().tweak_data] then
-			if pickup:pickup() and pickup:pickup():pickup(self._unit) then
-				for id, weapon in pairs(self._unit:inventory():available_selections()) do
-					managers.hud:set_ammo_amount(id, weapon.unit:base():ammo_info())
+function PlayerStandard:_find_pickups(list)
+	local pickups = World:find_units_quick("sphere", self._unit:movement():m_pos(), 200) or {}
+	if pickups and #pickups > 0 then
+		for _, pickup in pairs(pickups) do
+			if pickup:interaction() and pickup:interaction().tweak_data then
+				if list[pickup:interaction().tweak_data] and pickup:pickup() and pickup:pickup():pickup(self._unit) then
+					for id, weapon in pairs(self._unit:inventory():available_selections()) do
+						managers.hud:set_ammo_amount(id, weapon.unit:base():ammo_info())
+					end
 				end
 			end
 		end
 	end
 end
 
-Hooks:PostHook(PlayerStandard, "_update_check_actions", "F_"..Idstring("Post.PlyStand._update_check_actions.Auto Pickup Ammo"):key(), function(self, t, dt)
+Hooks:PostHook(PlayerStandard, "_update_check_actions", "F_"..Idstring("Post.PlyStand._update_check_actions.Auto Pickup Ammo"):key(), function(self)
 	if self._find_pickups_dt then
-		self._find_pickups_dt = self._find_pickups_dt - dt
-		if self._find_pickups_dt < 0 then
-			self._find_pickups_dt = nil
-		end
+		self._find_pickups_dt = nil
 	else
-		self._find_pickups_dt = 0.25
-		self:_find_pickups(t, {ammo_bag = true, ammo_bag_small = true})
+		self._find_pickups_dt = true
+		self:_find_pickups({ammo_bag = true, ammo_bag_small = true})
 	end
 end)
