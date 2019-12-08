17e84a86a1b8906650c2fdc07dd512fe977b3c0a
Dr_Newbie <tf2baidonation@gmail.com>
2019-12-08 19:29:53 +0800

    Add Armor
    http://modwork.shop/26122

---
+++ Mods/Add_Armor/Hooks/playertweakdata.lua
@@ -0,0 +1,3 @@
+Hooks:PostHook(PlayerTweakData, "_init_default_class_tweak_data", "F_"..Idstring("PostHook:PlayerTweakData:_init_default_class_tweak_data:Add Armor"):key(), function(self)
+	self.class_defaults.default.damage.BASE_ARMOR = 20
+end)
