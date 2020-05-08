7075d18658d76292176bb530be1473b2976301aa
Dr_Newbie <tf2baidonation@gmail.com>
2020-05-08 21:49:19 +0800

    Call an Airstrike ; 5

--- Mods/Call an Airstrike/distractionrock.lua
+++ Mods/Call an Airstrike/distractionrock.lua
@@ -1,6 +1,15 @@
-Hooks:PostHook(DistractionRock, "_detonate", "Airstrike_Check_Bomb_Now", function(self)
-	if not managers.groupai:state():whisper_mode() and self:get_name_id() == "decoy_coin" and not self._called_airstrike and self:thrower_unit() == managers.player:player_unit() then
-		self._called_airstrike = true
-		managers.barrage:spawn_flare(self._unit, self._unit)
-	end
-end)
+if not Network or Network:is_server() then
+	Hooks:PreHook(DistractionRock, "_detonate", "F_"..Idstring("PostHook:DistractionRock:_detonate:Call an Airstrike"):key(), function(self)
+		if not managers.groupai:state():whisper_mode() and self:get_name_id() == "decoy_coin" and not self._called_airstrike and self:thrower_unit() == managers.player:player_unit() then
+			self._called_airstrike = true
+			local __pos = self._unit:position()
+			local flare = safe_spawn_unit(BarrageManager.FLARE_UNIT, __pos, Rotation())
+			if flare then
+				managers.barrage:sync_spotter_spawn_flare(flare, __pos, Rotation(), math.UP, 1)
+				if managers.network then
+					managers.network:session():send_to_peers_synched("sync_spotter_spawn_flare", flare, __pos, Rotation(), math.UP)
+				end
+			end
+		end
+	end)
+end
